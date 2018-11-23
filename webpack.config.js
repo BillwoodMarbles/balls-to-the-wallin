540597c4ee77448e3a6d0f76a24cbd6e8488a9d0
Harrison <harrison@1bg.com>
2018-11-23 12:31:29 -0700

    favicon fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 const publicUrl = '/';
 
@@ -69,6 +70,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css"
-    })
+    }),
+      new CopyWebpackPlugin([
+          // relative path is from src
+          { from: './public/favicon.ico' }, // <- your path to favicon
+      ])
   ]
 };
